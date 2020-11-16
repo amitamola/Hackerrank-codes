793425f1956e5684ef1ddc3d2c2addb2bd04dc0a
Amit Amola <amitamola.dun@gmail.com>
2020-11-16 10:39:48 +0530

    Create itertools_combinations_with_replacement.py

---
+++ itertools_combinations_with_replacement.py
@@ -0,0 +1,42 @@
+'''
+Task
+
+You are given a string .
+Your task is to print all possible size  replacement combinations of the string in lexicographic sorted order.
+
+Input Format
+
+A single line containing the string  and integer value  separated by a space.
+
+Constraints
+
+
+The string contains only UPPERCASE characters.
+
+Output Format
+
+Print the combinations with their replacements of string  on separate lines.
+
+Sample Input
+
+HACK 2
+Sample Output
+
+AA
+AC
+AH
+AK
+CC
+CH
+CK
+HH
+HK
+KK
+'''
+
+from itertools import combinations_with_replacement
+word, r = input().split()
+
+to_p = tuple(combinations_with_replacement(sorted(word), int(r)))
+
+for word in to_p: print(''.join(word))
