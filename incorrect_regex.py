3e509addf4573882952dc8f9051937f3c698b1d8
Amit Amola <amitamola.dun@gmail.com>
2020-12-10 14:14:56 +0530

    Create incorrect_regex.py

---
+++ incorrect_regex.py
@@ -0,0 +1,39 @@
+'''
+You are given a string .
+Your task is to find out whether  is a valid regex or not.
+
+Input Format
+
+The first line contains integer , the number of test cases.
+The next  lines contains the string .
+
+Constraints
+
+
+Output Format
+
+Print "True" or "False" for each test case without quotes.
+
+Sample Input
+
+2
+.*\+
+.*+
+Sample Output
+
+True
+False
+Explanation
+
+.*\+ : Valid regex.
+.*+: Has the error multiple repeat. Hence, it is invalid.
+'''
+
+import re
+for _ in range(int(input())):
+    ans = True
+    try:
+        reg = re.compile(input())
+    except re.error:
+        ans = False
+    print(ans)
