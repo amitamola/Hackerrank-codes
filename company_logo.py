48c3888bc7aa8aad57873ab01ef22f638da3a0a4
Amit Amola <amitamola.dun@gmail.com>
2020-12-13 00:07:42 +0530

    Create company_logo.py

---
+++ company_logo.py
@@ -0,0 +1,40 @@
+'''
+A newly opened multinational brand has decided to base their company logo on the three most common characters in the company name. They are now trying out various combinations of company names and logos based on this condition. Given a string , which is the company name in lowercase letters, your task is to find the top three most common characters in the string.
+
+Print the three most common characters along with their occurrence count.
+Sort in descending order of occurrence count.
+If the occurrence count is the same, sort the characters in alphabetical order.
+For example, according to the conditions described above,
+
+ would have it's logo with the letters .
+
+Input Format
+
+A single line of input containing the string .
+
+Constraints
+
+Output Format
+
+Print the three most common characters along with their occurrence count each on a separate line.
+Sort output in descending order of occurrence count.
+If the occurrence count is the same, sort the characters in alphabetical order.
+
+Sample Input 0
+
+aabbbccde
+Sample Output 0
+
+b 3
+a 2
+c 2
+'''
+
+from collections import Counter
+
+if __name__ == '__main__':
+    s = sorted(input())
+
+    aly = Counter(s).most_common(3)
+    for val in aly:
+        print(val[0], val[1])
