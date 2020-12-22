91bcb24b19768555282e6d8f500a950604c4dca9
Amit Amola <amitamola.dun@gmail.com>
2020-12-22 22:01:23 +0530

    Create Cats and a Mouse.py

---
+++ Algorithms/Cats and a Mouse.py
@@ -0,0 +1,44 @@
+'''
+https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the catAndMouse function below.
+def catAndMouse(x, y, z):
+    disA = abs(z-x)
+    
+    disB = abs(z-y)
+    
+    if disA==disB:
+        return "Mouse C"
+    
+    elif disA<disB:
+        return "Cat A"
+    
+    else:
+        return "Cat B"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
