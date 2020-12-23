dc591cfd867abe6b14fe744b00838da2e49cca5d
Amit Amola <amitamola.dun@gmail.com>
2020-12-23 23:30:52 +0530

    Update **Append and Delete.py

--- Algorithms/**Append and Delete.py
+++ Algorithms/**Append and Delete.py
@@ -7,33 +7,8 @@ import os
 import random
 import re
 import sys
-from difflib import ndiff
 
-# Complete the appendAndDelete function below.
 def appendAndDelete(s, t, k):
-    # s = list(s)
-    # t = list(t)
-
-    # if (s==t) and k>=2*len(s):
-    #     return "Yes"
-    
-    # elif (s!=t) and k>=(len(s)+len(t)):
-    #     return "Yes"
-    
-    # st = ''
-    # for val in list(ndiff(s, t)):
-    #     if val[0]==' ':
-    #         st+=val[2]
-    #     else:
-    #         break
-    
-    # if (k-len(s)-len(t)-2*len(st))%2==0:
-    # # if ((k - len(s) - len(t) + 2 *len(st)) % 2 == 0):
-    #     return "Yes"
-    
-    # else:
-    #     return "No"
-
     if ((len(s) + len(t)) < k): 
         return "Yes"
   
