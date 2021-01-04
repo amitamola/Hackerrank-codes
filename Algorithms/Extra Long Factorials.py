c78d287f83fe490069ea6532d1d99180cfe8e443
Amit Amola <amitamola.dun@gmail.com>
2021-01-04 18:12:49 +0530

    Create Extra Long Factorials.py

---
+++ Algorithms/Extra Long Factorials.py
@@ -0,0 +1,26 @@
+'''
+https://www.hackerrank.com/challenges/extra-long-factorials/problem
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    if n==1:
+        return 1
+    
+    else:
+        return n*extraLongFactorials(n-1)
+        
+
+if __name__ == '__main__':
+    n = int(input())
+
+    val = extraLongFactorials(n)
+    print(val)
