9b40dfc073872ad92743c7f706003851196ea797
Amit Amola <amitamola.dun@gmail.com>
2021-01-26 21:22:40 +0530

    Create Taum and B'day.py

---
+++ Algorithms/Taum and B'day.py
@@ -0,0 +1,45 @@
+'''
+https://www.hackerrank.com/challenges/taum-and-bday/problem
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+def taumBday(b, w, bc, wc, z):
+    if (bc+z)<wc:
+        return (bc+z)*w + bc*b
+    
+    elif (wc+z)<bc:
+        return (wc+z)*b + wc*w
+    
+    else:
+        return b*bc + w*wc
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        b = int(first_multiple_input[0])
+
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+
+        bc = int(second_multiple_input[0])
+
+        wc = int(second_multiple_input[1])
+
+        z = int(second_multiple_input[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
