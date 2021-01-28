ba190e620963599211dcde08b112751aab603ad9
Amit Amola <amitamola.dun@gmail.com>
2021-01-29 00:38:57 +0530

    Create Two Strings.py

---
+++ Algorithms/Two Strings.py
@@ -0,0 +1,31 @@
+'''
+https://www.hackerrank.com/challenges/two-strings/problem
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+def twoStrings(s1, s2):
+    if len(set(s1) & set(s2))>0:
+        return 'YES'
+    else:
+        return 'NO'
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
