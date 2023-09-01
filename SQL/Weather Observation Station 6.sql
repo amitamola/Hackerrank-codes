5e559e62f9ce299f998155201351db1a2f78e8e0
Amit Amola <amitamola.dun@gmail.com>
2023-09-01 21:35:41 +0530

    Update Weather Observation Station 6.sql

--- SQL/Weather Observation Station 6.sql
+++ SQL/Weather Observation Station 6.sql
@@ -3,3 +3,8 @@
 SELECT DISTINCT CITY
 FROM STATION
 WHERE CITY REGEXP '^[aeiou]';
+
+-- Other ways:
+-- SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY, 1, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+-- select distinct(city) from station where city like 'A%' OR city like 'E%' OR city like 'I%' OR city like 'O%' OR city like 'U%' order by city ASC;
